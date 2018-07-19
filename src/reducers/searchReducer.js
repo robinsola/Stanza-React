19d85c6f5c5e4b36c544c86a981fa94a310dddd7
Robin Sola <rsolarms@gmail.com>
2018-07-19 14:32:08 -0700

    create FETCH_POEMS in search reducer

--- src/reducers/searchReducer.js
+++ src/reducers/searchReducer.js
@@ -2,10 +2,15 @@ import { FETCH_POEMS } from '../actions/types';
 
 const initialState = {
   results: [],
-}
+};
 
 export default function(state = initialState, action) {
   switch(action.type) {
+    case FETCH_POEMS:
+      return {
+        ...state,
+        results: action.payload
+      }
     default:
       return state;
   }
