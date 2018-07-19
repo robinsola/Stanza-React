9a816c42f52ef4b469f64901ad5fc7c57906486d
Robin Sola <rsolarms@gmail.com>
2018-07-19 14:30:20 -0700

    add Provider and import store

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Navbar from './components/Navbar';
-import Splash from './components/Splash';
-import SearchForm from './components/SearchForm';
 import { Link, Route, Switch } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './store';
+import Navbar from './components/Navbar';
+import Splash from './components/Splash';
+import SearchForm from './components/SearchForm';
 
 class App extends Component {
   render() {
