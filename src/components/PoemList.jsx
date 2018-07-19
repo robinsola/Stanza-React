e12cfcc5041793ffae3baec30cabb4087e56439b
Robin Sola <rsolarms@gmail.com>
2018-07-19 14:33:45 -0700

    remove api call and add mapStateToProps to import search results from new state

--- src/components/PoemList.jsx
+++ src/components/PoemList.jsx
@@ -1,54 +1,34 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { fetchPoems } from '../actions/searchAction';
+import PropTypes from 'prop-types';
 
 class PoemList extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      poemList: [],
-      requestFiled: false
-    }
-  }
 
   componentDidMount() {
-    console.log('test');
-    fetch('https://cors-anywhere.herokuapp.com/' + 'http://poetrydb.org/author,title/dickinson;death')
-    .then(response => {
-      if (!response.ok) {
-        throw Error('Sorry, request has failed')
-      }
-      return response
-    })
-    .then(response => response.json())
-    .then(data => {
-      this.setState({
-        poemList: data
-      })
-    }, () => {
-      this.setState({
-        requestFailed: true
-      })
-    })
+    this.props.fetchPoems();
   }
 
   render() {
 
-    if(this.state.requestFailed) return <p>failed!</p>
-    if(!this.state.poemList) return <p>Loading...</p>
-
-    const searchResults = this.state.poemList.map(poem => (
+    const searchResults = this.props.poemList.map(poem => (
       <div key={poem.id}>
         <h2>{poem.title}</h2>
         <h4>{poem.author}</h4>
         <p>{poem.lines}</p>
       </div>
-    ))
+    ));
     return (
       <div>
         <p>Poem list: Search results from api call</p>
         {searchResults}
       </div>
-    );
+    )
   }
 }
 
-export default PoemList;
+const mapStateToProps = state => ({
+  poemList: state.poemList.results
+});
+
+export default connect(mapStateToProps, { fetchPoems })(PoemList);
