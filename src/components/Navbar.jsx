cacef68206812ace763763748192ac9c780c835d
Robin Sola <rsolarms@gmail.com>
2018-07-20 09:00:51 -0700

    add navbar icons

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import searchIcon from '../assets/search_gray.png';
+import shuffleIcon from '../assets/shuffle_gray.png';
+import favsIcon from '../assets/fave_gray.png';
 
 function Navbar() {
   return (
     <div>
       <p>navigation bar</p>
-      <Link to="/">Splash</Link> | <Link to="/searchform">SearchForm</Link>
+      <Link to="/">Splash</Link>
+      <Link to="/searchform"><img src={searchIcon}/></Link>
+      <Link to="/shufflepoem"><img src={shuffleIcon}/></Link>
+      <Link to="/favorites"><img src={favsIcon}/></Link>
     </div>
   );
 }
