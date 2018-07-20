d66c94ec7a49d170701879aa41363d5205d5fff8
Robin Sola <rsolarms@gmail.com>
2018-07-20 13:18:42 -0700

    add styles to enter btn

--- src/components/Splash.jsx
+++ src/components/Splash.jsx
@@ -4,6 +4,10 @@ import './Splash.css';
 import feather from '../assets/feather.png';
 import inkWell from '../assets/logo2.png';
 function Splash() {
+  const linkStyles = {
+    textDecoration: 'none',
+    color: 'white'
+  };
   return (
     <div className='splashContainer'>
       <div className='logoStyles'>
@@ -13,7 +17,7 @@ function Splash() {
         <div className='inkWell'>
           <img src={inkWell}/>
         </div>
-        <Link className='enterBtn' to="/searchform">find poems</Link>
+        <Link className='enterBtn' style={linkStyles} to="/searchform">find poems</Link>
       </div>
     </div>
   );
