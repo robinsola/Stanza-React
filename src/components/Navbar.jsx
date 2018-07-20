c13311edc405cae225aa71643c0f904cdc05eb29
Robin Sola <rsolarms@gmail.com>
2018-07-20 09:25:57 -0700

    add nav dividers and flex

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,15 +3,14 @@ import { Link } from 'react-router-dom';
 import searchIcon from '../assets/search_gray.png';
 import shuffleIcon from '../assets/shuffle_gray.png';
 import favsIcon from '../assets/fave_gray.png';
+import './Navbar.css';
 
 function Navbar() {
   return (
-    <div>
-      <p>navigation bar</p>
-      <Link to="/">Splash</Link>
+    <div className='navStyles'>
       <Link to="/searchform"><img src={searchIcon}/></Link>
-      <Link to="/shufflepoem"><img src={shuffleIcon}/></Link>
-      <Link to="/favorites"><img src={favsIcon}/></Link>
+      <Link to="/"><img className='navDividers' src={shuffleIcon}/></Link>
+      <Link to="/"><img src={favsIcon}/></Link>
     </div>
   );
 }
