84781987a93671ae45f04070379a2b5604ee4eed
Robin Sola <rsolarms@gmail.com>
2018-07-20 09:39:15 -0700

    import logo images

--- src/components/Splash.jsx
+++ src/components/Splash.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-
+import feather from '../assets/feather.png';
+import inkWell from '../assets/logo2.png';
 function Splash() {
   return (
     <div>
-      <p>Logo splash page</p>
-      <Link to="/searchform">SearchForm</Link>
+      <div>
+        <img src={feather}/>
+        <img src={inkWell}/>
+      </div>
+      <Link to="/searchform">find poems</Link>
     </div>
   );
 }
