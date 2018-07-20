d1c5f4ccb1b45b20f046df6fec0617aa7c339092
Robin Sola <rsolarms@gmail.com>
2018-07-20 14:03:55 -0700

    add radio buttons

--- src/components/SearchForm.jsx
+++ src/components/SearchForm.jsx
@@ -15,11 +15,30 @@ function SearchForm() {
     <div>
       <Navbar />
       <div className='formStyles'>
+        <h3>What's the Occasion?</h3>
         <form onSubmit={handleSearch}>
           <div className='radioGroup'>
-            <label><input type='radio' name='radioInput' value='romance'/>Love</label>
+            <label><input type='radio' name='radioInput' value='anniversary'/>Anniversary</label>
             <br />
-            <label><input type='radio' name='radioInput' value='death'/>Death</label>
+            <label><input type='radio' name='radioInput' value='birthday'/>Birthday</label>
+            <br />
+            <label><input type='radio' name='radioInput' value='breakup'/>Breakup</label>
+            <br />
+            <label><input type='radio' name='radioInput' value='romance'/>Casual Hookup</label>
+            <br />
+            <label><input type='radio' name='radioInput' value='congratulations'/>Congratulations</label>
+            <br />
+            <label><input type='radio' name='radioInput' value='farewell'/>Farewell</label>
+            <br />
+            <label><input type='radio' name='radioInput' value='father'/>Father's Day</label>
+            <br />
+            <label><input type='radio' name='radioInput' value='halloween'/>Halloween</label>
+            <br />
+            <label><input type='radio' name='radioInput' value='mother'/>Mother's Day</label>
+            <br />
+            <label><input type='radio' name='radioInput' value='spring'/>Spring</label>
+            <br />
+            <label><input type='radio' name='radioInput' value='winter'/>Winter</label>
             <br />
           </div>
           <input type='text' id='author' placeholder='search by author' ref={(input)=>{_author = input;}}/><br />
