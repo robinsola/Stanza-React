5912a134d0b91be6a09db41e2f9243300725b681
Robin Sola <rsolarms@gmail.com>
2018-07-21 17:33:47 -0700

    add styles to navbar and import flatLogo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,14 +3,18 @@ import { Link } from 'react-router-dom';
 import searchIcon from '../assets/search_active.png';
 import shuffleIcon from '../assets/shuffle_active.png';
 import favsIcon from '../assets/favs_active.png';
+import flatLogo from '../assets/flat_logo.png';
 import './Navbar.css';
 
 function Navbar() {
   return (
-    <div className='navStyles'>
-      <Link to="/searchform"><img src={searchIcon}/></Link>
-      <Link to="/"><img className='navDividers' src={shuffleIcon}/></Link>
-      <Link to="/"><img src={favsIcon}/></Link>
+    <div>
+      <div className='navStyles'>
+        <Link to="/searchform"><img src={searchIcon}/></Link>
+        <Link to="/"><img className='navDividers' src={shuffleIcon}/></Link>
+        <Link to="/"><img src={favsIcon}/></Link>
+      </div>
+      <img className="flatLogo" src={flatLogo}/>
     </div>
   );
 }
