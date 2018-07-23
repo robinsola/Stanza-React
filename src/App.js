12149ac1b6435309d35ea3d8541ddadce1dac887
Robin Sola <rsolarms@gmail.com>
2018-07-23 09:14:33 -0700

    move Provider to index

--- src/App.js
+++ src/App.js
@@ -2,8 +2,6 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { Link, Route, Switch } from 'react-router-dom';
-import { Provider } from 'react-redux';
-import store from './store';
 import Navbar from './components/Navbar';
 import Splash from './components/Splash';
 import SearchForm from './components/SearchForm';
@@ -11,14 +9,12 @@ import SearchForm from './components/SearchForm';
 class App extends Component {
   render() {
     return (
-      <Provider store={store}>
         <div className="App">
           <Switch>
             <Route exact path = '/' component={Splash} />
             <Route path = '/searchform' component={SearchForm} />
           </Switch>
         </div>
-      </Provider>
     );
   }
 }
