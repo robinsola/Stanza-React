fe60b84fbff0ad36f151690f0d11f5241b7e862b
Robin Sola <rsolarms@gmail.com>
2018-07-23 14:12:49 -0700

    remove this. from props and connect to store

--- src/components/SearchForm.jsx
+++ src/components/SearchForm.jsx
@@ -1,85 +1,73 @@
 import React from 'react';
-import PoemList from './PoemList';
 import Navbar from './Navbar';
 import './searchForm.css';
+import {Link} from 'react-router-dom';
 import {connect} from 'react-redux';
-import PropTypes from 'prop-types';
 
-class SearchForm extends React.Component {
+function SearchForm(props) {
 
-  onKeywordChange = (e) => {
-    let {dispatch} = this.props;
+  function onAuthorChange(e) {
+    let {dispatch} = props;
+    dispatch({type: 'SEARCH_AUTHOR', inputAuthor: e.target.value})
+  }
+
+  function onKeywordChange(e) {
+    let {dispatch} = props;
     dispatch({type: 'SEARCH_KEYWORD', inputKeyword: e.target.value})
   }
 
-  fetchPoems = () => {
-    let {dispatch} = this.props;
-    let {inputKeyword} = this.props;
-    fetch(`http://poetrydb.org/title/${inputKeyword}`)
+  function onSearch(e) {
+    let {dispatch} = props;
+    let {inputAuthor} = props;
+    let {inputKeyword} = props;
+    fetch(`http://poetrydb.org/author,title/${inputAuthor};${inputKeyword}`)
       .then(response => {
         return response.json()
       })
       .then(response => {
-        dispatch({type: 'FETCH_POEMS', results: response})
+        dispatch({type: 'FETCH_POEMS', searchResults: response})
       })
   }
 
-  render () {
-    const poemList = this.props.poems.map(poem => (
-      <div key={poem.id}>
-        <h2>{poem.title}</h2>
-        <h4>{poem.author}</h4>
-        <pre>{poem.lines.join('\n')}</pre>
+  return (
+    <div>
+      <Navbar />
+      <div className='formStyles'>
+        <form>
+          <h3 className="title">pick a theme:</h3>
+          <div className='radioGroup'>
+            <label><input type='radio' value='anniversary' onChange={onKeywordChange} checked={props.inputKeyword === 'anniversary'}/><span>Anniversary</span></label>
+            <br />
+            <label><input type='radio' value='birthday' onChange={onKeywordChange} checked={props.inputKeyword === 'birthday'}/><span>Birthday</span></label>
+            <br />
+            <label><input type='radio' value='farewell' onChange={onKeywordChange} checked={props.inputKeyword === 'farewell'}/><span>Breakup</span></label>
+            <br />
+            <label><input type='radio' value='romance' onChange={onKeywordChange} checked={props.inputKeyword === 'romance'}/><span>Casual Hookup</span></label>
+            <br />
+            <label><input type='radio' value='celebrate' onChange={onKeywordChange} checked={props.inputKeyword === 'celebrate'}/><span>Congratulations</span></label>
+            <br />
+            <label><input type='radio' value='father' onChange={onKeywordChange} checked={props.inputKeyword === 'father'}/><span>Fathers Day</span></label>
+            <br />
+            <label><input type='radio' value='death' onChange={onKeywordChange} checked={props.inputKeyword === 'death'}/><span>Funeral</span></label>
+            <br />
+            <label><input type='radio' value='fear' onChange={onKeywordChange} checked={props.inputKeyword === 'fear'}/><span>Halloween</span></label>
+            <br />
+            <label><input type='radio' value='mother' onChange={onKeywordChange} checked={props.inputKeyword === 'mother'}/><span>Mothers Day</span></label>
+            <br />
+            <label><input type='radio' value='spring' onChange={onKeywordChange} checked={props.inputKeyword === 'spring'}/><span>Spring</span></label>
+            <br />
+            <label><input type='radio' value='thank' onChange={onKeywordChange} checked={props.inputKeyword === 'thank'}/><span>Thank You</span></label>
+            <br />
+            <label><input type='radio' value='love' onChange={onKeywordChange} checked={props.inputKeyword === 'love'}/><span>Wedding</span></label>
+          </div>
+          <h3 className="title">or choose your own:</h3>
+          <input type='text' id='author' placeholder='search by author'onChange={onAuthorChange} value={props.inputAuthor} /><br />
+          <input type='text' id='theme' placeholder='search by theme'/><br />
+          <Link to='/'><button onClick={onSearch}>find poems</button></Link>
+        </form>
       </div>
-    ));
-    return (
-      <div>
-        <Navbar />
-        <div className='formStyles'>
-          <form>
-            <h3 className="title">pick a theme:</h3>
-            <div className='radioGroup'>
-              <label><input type='radio' value='anniversary' onChange={this.onKeywordChange} checked={this.props.inputKeyword === 'anniversary'}/><span>Anniversary</span></label>
-              <br />
-              <label><input type='radio' value='birthday' onChange={this.onKeywordChange} checked={this.props.inputKeyword === 'birthday'}/><span>Birthday</span></label>
-              <br />
-              <label><input type='radio' value='farewell' onChange={this.onKeywordChange} checked={this.props.inputKeyword === 'farewell'}/><span>Breakup</span></label>
-              <br />
-              <label><input type='radio' value='romance' onChange={this.onKeywordChange} checked={this.props.inputKeyword === 'romance'}/><span>Casual Hookup</span></label>
-              <br />
-              <label><input type='radio' value='celebrate' onChange={this.onKeywordChange} checked={this.props.inputKeyword === 'celebrate'}/><span>Congratulations</span></label>
-              <br />
-              <label><input type='radio' value='father' onChange={this.onKeywordChange} checked={this.props.inputKeyword === 'father'}/><span>Father's Day</span></label>
-              <br />
-              <label><input type='radio' value='death' onChange={this.onKeywordChange} checked={this.props.inputKeyword === 'death'}/><span>Funeral</span></label>
-              <br />
-              <label><input type='radio' value='fear' onChange={this.onKeywordChange} checked={this.props.inputKeyword === 'fear'}/><span>Halloween</span></label>
-              <br />
-              <label><input type='radio' value='mother' onChange={this.onKeywordChange} checked={this.props.inputKeyword === 'mother'}/><span>Mother's Day</span></label>
-              <br />
-              <label><input type='radio' value='spring' onChange={this.onKeywordChange} checked={this.props.inputKeyword === 'spring'}/><span>Spring</span></label>
-              <br />
-              <label><input type='radio' value='thank' onChange={this.onKeywordChange} checked={this.props.inputKeyword === 'thank'}/><span>Thank You</span></label>
-              <br />
-              <label><input type='radio' value='love' onChange={this.onKeywordChange} checked={this.props.inputKeyword === 'love'}/><span>Wedding</span></label>
-              <br />
-              <label><input type='radio' value='winter' onChange={this.onKeywordChange} checked={this.props.inputKeyword === 'winter'}/><span>Winter</span></label>
-              <br />
-            </div>
-            <h3 className="title">or choose your own:</h3>
-            <input type='text' id='author' placeholder='search by author'/><br />
-            <input type='text' id='theme' placeholder='search by theme'/><br />
-            <button onClick={this.fetchPoems}>find poems</button>
-          </form>
-          <p>{poemList}</p>
-        </div>
-      </div>
-    );
-  }
+    </div>
+  );
 }
 
-SearchForm.propTypes = {
-  poems: PropTypes.object
-};
-
-export default SearchForm;
+export default connect()(SearchForm);
